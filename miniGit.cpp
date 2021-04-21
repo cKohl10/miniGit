eab2b355cfc26821517abf93a96731c0b32252b6
aiba4531 <aiba4531@colorado.edu>
2021-04-21 17:02:39 -0600

    remove function first implementation

--- miniGit.cpp
+++ miniGit.cpp
@@ -19,9 +19,27 @@ void Master::add()
 {
 
 }
-void Master::remove()
+void Master::remove(string filename)
 {
-
+    singlyNode* ptrToDelete = commitHead->head; //pointer to traverse is equal to the head of the singly linked list
+    singlyNode* prev = NULL;
+    bool found = false;
+    while (ptrToDelete != NULL)
+    {
+        if (ptrToDelete->fileName == filename)
+        {
+            found = true;
+            break;
+        }
+        prev = ptrToDelete;
+        ptrToDelete = ptrToDelete->next;
+    }
+    if (found == true) //if the file has been found
+    {
+        prev->next = ptrToDelete->next;
+        delete ptrToDelete;
+    }
+    
 }
 void Master::checkout()
 {
