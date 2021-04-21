6697d6a5a52c223ea4443360f5be25ccc2cee374
henry-dyer <hmdyer@gmail.com>
2021-04-21 16:34:22 -0600

    Adding functions to implementation

--- miniGit.cpp
+++ miniGit.cpp
@@ -1,3 +1,24 @@
 #include "miniGit.hpp"
 
 using namespace std;
+
+void Master::init()
+{
+
+}
+void Master::add()
+{
+
+}
+void Master::remove()
+{
+
+}
+void Master::checkout()
+{
+
+}
+void Master::commit()
+{
+    
+}
